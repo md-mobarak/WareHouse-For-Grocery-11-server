c1d7696e6734e993fbda6bedd190398a2f2dd074
mohammad-mobarak-hossen <mdmobarakhossen112@gmail.com>
2022-05-03 23:46:33 +0600

    get load data by dynamic

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
@@ -11,15 +12,37 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://bduser:hDrsnHr8dZoOzyUE@cluster0.vv6vo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vv6vo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("groci").collection("product");
-    // perform actions on the collection object
-    client.close();
-    console.log('server is runiiiiiiiiii');
-});
+console.log('kkkkk');
+async function run() {
+    try {
+
+        await client.connect()
+        const productCollection = client.db('groci').collection('product')
+        app.get('/product', async (req, res) => {
+            const query = {}
+            const cursor = productCollection.find(query)
+            const product = await cursor.toArray()
+            res.send(product)
+        })
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const product = await productCollection.findOne(query)
+            console.log(product);
+            res.send(product)
+        })
+
+
+    }
+
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
