4f33f89d562c923bda764e1ffc731819abb4dc08
mohammad-mobarak-hossen <mdmobarakhossen112@gmail.com>
2022-05-07 22:49:33 +0600

    my item post & delete is complete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -11,7 +12,11 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
-
+function varifyJwt(req, res, next) {
+    const auth = req.headers.authorization;
+    console.log(auth);
+    next()
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vv6vo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -22,6 +27,19 @@ async function run() {
         await client.connect()
         const productCollection = client.db('groci').collection('product')
         const itemCollection = client.db('item').collection('newItem')
+
+
+        //   AUTH 
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
+
+
+        // SERVICES
         app.get('/product', async (req, res) => {
             const query = {}
             const cursor = productCollection.find(query)
@@ -42,6 +60,15 @@ async function run() {
             const result = await productCollection.deleteOne(query)
             res.send(result)
         })
+
+        app.delete('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await itemCollection.deleteOne(query)
+            console.log(result);
+            res.send(result)
+        })
+
         app.put('/product/:id', async (req, res) => {
             const id = req.params.id
             const updatedProduct = req.body;
@@ -63,8 +90,16 @@ async function run() {
             // console.log(newProduct);
             res.send(result)
         })
+        app.post('/manageproduct', async (req, res) => {
+            const newProduct = req.body;
+            // const result = await productCollection.insertOne(newProduct)
+            const result = await productCollection.insertOne(newProduct)
+            // console.log(newProduct);
+            res.send(result)
+        })
 
-        app.get('/myitem', async (req, res) => {
+        app.get('/myitem', varifyJwt, async (req, res) => {
+
             const email = req.query.email;
             // console.log(email);
             const query = { email: email }
